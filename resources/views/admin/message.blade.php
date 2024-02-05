60742540b78d7acac31877719151e4eb2d8d784b
chrazesh <chrazesh3@gmail.com>
2024-02-05 15:53:07 +0545

    link dashboard page in sidebar

--- resources/views/admin/message.blade.php
+++ resources/views/admin/message.blade.php
@@ -1,7 +1,7 @@
 @if (Session::has('error'))
 <div class="alert alert-danger alert-dismissible">
     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-    <h4><i class="icon fa fa-ban"></i> Error!</h4>{{ Session::get('error')}}
+    <h4><i class="icon fa fa-ban"></i> Error!</h4> {{ Session::get('error')}}
 </div>
 @endif
 
@@ -11,4 +11,4 @@
     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
     <h4><i class="icon fa fa-check"></i> Success!</h4> {{ Session::get('success') }}
     </div>
-    @enderror 
+    @endif
